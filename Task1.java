b29664d135e548a11584409f10d095eb4f425362
arnoor-1 <arnoor713@gmail.com>
2025-10-29 22:05:23 +0600

    25-62789-2

---
+++ Task1.java
@@ -0,0 +1,11 @@
+public class Task1 {
+   public static void main (String[] args){
+       int x = 75;
+       int y = 82;
+       int z = 80;
+       int sum = x + y + z;
+       System.out.println("Sum is : " + sum + " Average is : " + (sum/3));
+
+
+}
+}
---
+++ Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+    public static void main (String[]args){
+        int price = 250 ;
+        System.out.println("Before discount price: " +price);
+        double discount = (price*0.15);
+        System.out.println("After 15% discount price: " +discount);
+    }
+}
