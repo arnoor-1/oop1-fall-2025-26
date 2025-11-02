491c26b7dd7fa1ae6eaf5ae0ba7a6ed01162afc9
arnoor-1 <arnoor713@gmail.com>
2025-11-02 21:18:01 +0600

    25-62789-2

---
+++ Lab/Task3.java
@@ -0,0 +1,7 @@
+public class Task3 {
+    public static void main(String[]args){
+        int celcius = 27;
+        double farhenheit = 32 + (celcius*(180/100));
+        System.out.println("Celcius: " + celcius + " degree= " + farhenheit + " Farenheit");
+    }
+}
