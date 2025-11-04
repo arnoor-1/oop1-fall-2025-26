5c81e602beeca64adc5a4538fe95698e3d55d578
arnoor-1 <arnoor713@gmail.com>
2025-11-04 21:22:21 +0600

    25-62789-2

--- Project/MortgageCalculator.java
+++ Project/MortgageCalculator.java
@@ -1,22 +1,31 @@
+import java.util.Scanner; 
 
-import java.util.Scanner ;
-public class MortgageCalculator {
+public  class MortgageCalculator {
+    public static void main (String[]args ) {
+   System.out.println ("Mortgage Calculator: ") ;
 
-    public static void main (String[]args) {
-        System.out.println ("Mortgage Calculator") ;
-        Scanner input = new Scanner(System.in) ;
-        System.out.println("Enter loan ammount: ");
-        int M = input.nextInt();
-        System.out.println("Enter the inrest rate: ") ;
-        float P = input.nextFloat();
-        System.out.println("Enter the loan period time in years : ") ;
-        int years = input.nextInt();
-        double r = years/100/12;
-        int n = years*12 ;
-        double numerator = r * Math.pow (1+r, n);
-        double denominator = Math.pow (1+r, n)-1;
-        double M = (numerator/denominator);
-        System.out.println(Total amount: + M);
+
+   Scanner input = new Scanner (System.in) ;
+
+
+   System.out.println("Enter the loan amount (P): ") ;
+   double principal = input.nextDouble() ;
+
+   System.out.println("Enter annual interest rate in ( in % ): ");
+   double annualRate = input.nextDouble();
+
+   System.out.println("Enter the loan term in years : ");
+     int years = input.nextInt();
+     double r = annualRate / 100/ 12 ;
+      int n =  years * 12 ;
+
+      double numerator = r * Math.pow( 1+r , n );
+      double denominator = Math .pow (1+r,n)- 1 ;
+        double monthlyPayment = principal * (numerator/denominator );
+
+        System.out.println ( "Your monthly payment is ($) : " + String.format("%.2f", monthlyPayment));
+
+        input.close();
 
     }
 }
