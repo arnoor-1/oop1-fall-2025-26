d58b7cd2492dc02b63ff4c65ac3b6eee73c136ad
arnoor-1 <arnoor713@gmail.com>
2025-11-06 23:14:50 +0600

    25-62789-2

--- Task3.java
+++
@@ -1,7 +0,0 @@
-public class Task3 {
-    public static void main(String[]args){
-        int celcius = 27;
-        double farhenheit = 32 + (celcius*(180/100));
-        System.out.println("Celcius: " + celcius + " degree= " + farhenheit + " Farenheit");
-    }
-}
